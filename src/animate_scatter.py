996ce9fa45a02055104b13b4248053aff19ef7b3
Michał Piekarski <michalpiekars@gmail.com>
2020-12-17 22:35:49 +0100

    formatting

--- src/animate_scatter.py
+++ src/animate_scatter.py
@@ -1,16 +1,18 @@
 import time
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-class AnimateScatter():
+
+class AnimateScatter:
     """creates an animated scatter plot which can be updated"""
+
     def __init__(self, xmin, xmax, ymin, ymax, pos, col, func, resolution, t):
         plt.ion()
 
-        self.xmin = xmin 
-        self.xmax = xmax        
-        self.ymin = ymin        
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
         self.ymax = ymax
 
         self.fig, self.ax = plt.subplots()
@@ -18,17 +20,17 @@ class AnimateScatter():
         self.c = col
         self.func = func
         self.t = t
-    
-        #add resolution to eliminate whitespace
-        self.x = np.arange(self.xmin, self.xmax+resolution, resolution)
-        self.y = np.arange(self.ymin, self.ymax+resolution, resolution)
+
+        # add resolution to eliminate whitespace
+        self.x = np.arange(self.xmin, self.xmax + resolution, resolution)
+        self.y = np.arange(self.ymin, self.ymax + resolution, resolution)
         xx, yy = np.meshgrid(self.x, self.y, sparse=True)
-        self.z = self.func(xx,yy)
+        self.z = self.func(xx, yy)
         self.update(pos)
 
     def draw_background(self):
         """draw filled contour of function meshgrid"""
-        self.ax.contourf( self.x, self.y, self.z)
+        self.ax.contourf(self.x, self.y, self.z)
 
     def update_canvas(self):
         self.fig.canvas.draw()
@@ -38,8 +40,6 @@ class AnimateScatter():
         self.ax.clear()
         self.ax.axis([self.xmin, self.xmax, self.ymin, self.ymax])
         self.draw_background()
-        self.ax.scatter(pos[:,0], pos[:,1], s=30, c=self.c)
+        self.ax.scatter(pos[:, 0], pos[:, 1], s=30, c=self.c)
         self.update_canvas()
         time.sleep(self.t)
-
-
